4cc964b4749cf7025fab999636755334a5fa6946
Thuy Kobe <112232711+thuynguyentrungdang@users.noreply.github.com>
2025-12-06 09:47:27 +0700

    Updated to 4.0 (#2)
    * Initial commit
    
    * Added EpicTraderHelper.cs

---
+++ Painter-4.0/Painter-4.0/Painter.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Spt.Config;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using SPTarkov.Server.Core.Models.Utils;
+using SPTarkov.Server.Core.Routers;
+using SPTarkov.Server.Core.Servers;
+using SPTarkov.Server.Core.Utils;
+using Path = System.IO.Path;
+
+namespace Painter;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.emilanderss0n.painter";
+    public override string Name { get; init; } = "Painter";
+    public override string Author { get; init; } = "MoxoPixel";
+    public override List<string>? Contributors { get; init; }
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; } = "";
+    public override bool? IsBundleMod { get; init; } = false;
+    public override string? License { get; init; } = "MIT";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+public class Painter(
+    ISptLogger<Painter> logger,
+    ModHelper modHelper,
+    ImageRouter imageRouter,
+    ConfigServer configServer,
+    TimeUtil timeUtil,
+    EpicTraderHelper traderHelper) : IOnLoad
+{
+    private readonly TraderConfig _traderConfig = configServer.GetConfig<TraderConfig>();
+    private readonly RagfairConfig _ragfairConfig = configServer.GetConfig<RagfairConfig>();
+    
+    public Task OnLoad()
+    {
+        // A path to the mods files we use below
+        var pathToMod = modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly());
+
+        // A relative path to the trader icon to show
+        var traderImagePath = Path.Combine(pathToMod, "res/painter.jpg");
+
+        // The base json containing trader settings we will add to the server
+        var traderBase = modHelper.GetJsonDataFromFile<TraderBase>(pathToMod, "db/base.json");
+
+        // Create a helper class and use it to register our traders image/icon + set its stock refresh time
+        imageRouter.AddRoute(traderBase.Avatar.Replace(".jpg", ""), traderImagePath);
+        traderHelper.SetTraderUpdateTime(_traderConfig, traderBase, timeUtil.GetHoursAsSeconds(1), timeUtil.GetHoursAsSeconds(2));
+
+        // Add our trader to the config file, this lets it be seen by the flea market
+        _ragfairConfig.Traders.TryAdd(traderBase.Id, true);
+
+        // Add our trader (with no items yet) to the server database
+        // An 'assort' is the term used to describe the offers a trader sells, it has 3 parts to an assort
+        // 1: The item
+        // 2: The barter scheme, cost of the item (money or barter)
+        // 3: The Loyalty level, what rep level is required to buy the item from trader
+        traderHelper.AddTraderWithEmptyAssortToDb(traderBase);
+
+        // Add localisation text for our trader to the database so it shows to people playing in different languages
+        traderHelper.AddTraderToLocales(traderBase, "Painter", "Painter is a trader who sells unique weapon mods and gear. Also painted weapon parts (incl. whole weapons).");
+
+        // Get the assort data from JSON
+        var assort = modHelper.GetJsonDataFromFile<TraderAssort>(pathToMod, "db/assort.json");
+
+        // Save the data we loaded above into the trader we've made
+        traderHelper.OverwriteTraderAssort(traderBase.Id, assort);
+        
+        logger.Success("[Painter] Mod loaded successfully.");
+        return Task.CompletedTask;
+    }
+}
